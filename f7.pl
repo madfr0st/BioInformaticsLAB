1c91317d491d85763bbabb0fe7c0ab71200f39d2
Suman Saurav <37533095+madfr0st@users.noreply.github.com>
2020-01-21 16:33:50 +0530

    Add files via upload

--- f7.pl
+++ f7.pl
@@ -2,9 +2,11 @@
 #to use array perl
 
 @array = ('a','b','c','d','e');
+
 $last = pop@array;
 print "popped element : ",$last,"\n";
 print "remaining array : ",@array, "\n";
+
 $first = shift@array;
 print "shifted element : ",$first,"\n";
 print "remaining array : ",@array, "\n";
@@ -14,5 +16,15 @@ print "adding last element at first : ",@array,"\n";
 
 push(@array,$first);
 print "adding first element at last : ",@array,"\n";
+
+@reverse = reverse@array;
+print "reversed array : ",@reverse,"\n";
+
+$length = scalar@array;
+print "array size : ",$length,"\n";
+
+$insert = '5';
+splice(@reverse,3,0,$insert);
+print "new Array : ",@reverse,"\n";
 
 exit;
